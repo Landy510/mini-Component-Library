0c73b35046324e013fc6f897f356ab56bb72ed35
Landy510 <randychen510@gmail.com>
2025-08-17 12:01:55 +0800

    perf: improve accessibility 1. Add VisuallyHidden in order to improve accessibility

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import styled from 'styled-components';
 
+import VisuallyHidden from "../VisuallyHidden";
 import { COLORS } from '../../constants';
 
 const Sizes = {
@@ -24,42 +25,37 @@ const Sizes = {
 const ProgressBar = ({ value, size }) => {
   const styles = Sizes[size];
   return (
-    <ProgressWrapper style={styles}>
-      <span 
-        id="loadinglabel"
-        style={{
-          display: "none"
-        }}
-      >Loading:</span>
-      <Progress 
-        role="progressbar"
-        aria-labelledby='loadinglabel'
-        aria-valuenow={value}
-        style={isFinished(value)} 
-        value={value}
-      ></Progress>
-    </ProgressWrapper>
+    <Wrapper style={styles}>
+      <VisuallyHidden>{value}</VisuallyHidden>
+      <ProgressbarWrapper>
+        <Progress 
+          role="progressbar"
+          aria-labelledby='loadinglabel'
+          aria-valuenow={value}
+          value={value}
+        ></Progress>
+      </ProgressbarWrapper>
+    </Wrapper>
   );
 };
 
-function isFinished(value) {
-  if(value < 100) return {"--borderRadius": "4px 0 0 4px"};
-  return {"--borderRadius": "4px"};
-}
-
-const ProgressWrapper = styled.div`
+const Wrapper = styled.div`
   height: var(--height);
   padding: var(--padding);
-  width: 370px;
   background-Color: ${COLORS.transparentGray15};
   border-radius: var(--borderRadius);
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  `
+  
+  const ProgressbarWrapper = styled.div`
+  height: 100%;
+  border-radius: 4px;
   overflow: hidden;
 `
+
 const Progress = styled.span`
   display: block;
   height: 100%;
-  border-radius: var(--borderRadius);
   width: ${p => p.value + "%"};
   background-color: ${COLORS.primary};
 `
